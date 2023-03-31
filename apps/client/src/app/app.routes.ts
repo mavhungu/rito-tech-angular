c0199d527d45b279a7e32e4c17d5a45b121b6df9
ronewa <ronewamavhungu@gmail.com>
2023-04-01 00:25:13 +0200

    add extension-summary router

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Route } from '@angular/router';
-import { CallListComponent, CallSummaryComponent, PageNotFoundComponent } from '@bvs-call-summary';
+import { CallListComponent, CallSummaryComponent,ExtensionSummaryComponent, PageNotFoundComponent } from '@bvs-call-summary';
 
 
 export const appRoutes: Route[] = [
   { path: '', component: CallListComponent},
   { path: 'summary/:id', component: CallSummaryComponent },
+  { path: 'extension-summary/:id', component: ExtensionSummaryComponent },
   { path: "**", component: PageNotFoundComponent }
+
 ];
--- libs/bvs-call-summary/src/index.ts
+++ libs/bvs-call-summary/src/index.ts
@@ -1,8 +1,8 @@
 export * from './lib/bvs-call-summary.module';
 
 export * from './lib/call-summary/call-summary.component';
-
 export * from './lib/call-list/call-list.component';
+export * from './lib/extension-summary/extension-summary.component';
 export * from './lib/page-not-found/page-not-found.component';
 export * from './lib/shared/title-strategy.service';
 export * from './lib/shared/api.service';
